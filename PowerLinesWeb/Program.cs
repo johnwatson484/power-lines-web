b93930f8c81528971a09764cd40022615f8e354e
John Watson <john.watson1@defra.gov.uk>
2025-03-29 19:31:38 +0000

    Add CSP

--- PowerLinesWeb/Program.cs
+++ PowerLinesWeb/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddScoped<IAccuracyApi, AccuracyApi>();
 
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(360);
+});
+
 var app = builder.Build();
 
 if (builder.Environment.IsDevelopment())
@@ -20,8 +27,33 @@ if (builder.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("Content-Security-Policy", string.Join(" ",
+        "font-src 'self' fonts.gstatic.com *.cloudflare.com *.fontawesome.com;",
+        "img-src 'self' *.google.com;",
+        "script-src 'self' 'unsafe-inline' code.jquery.com cdnjs.cloudflare.com *.bootstrapcdn.com *.fontawesome.com *.googletagmanager.com *.google.com;",
+        "style-src 'self' 'unsafe-inline' fonts.googleapis.com *.bootstrapcdn.com cdnjs.cloudflare.com code.jquery.com;",
+        "connect-src 'self' *.fontawesome.com;",
+        "frame-ancestors 'self';",
+        "form-action 'self';"
+    ));
+    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Append("X-Frame-Options", "DENY");
+    context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
+    context.Response.Headers.Append("Cache-Control", "no-cache");
+    context.Response.Headers.Append("Cross-Origin-Opener-Policy", "same-origin");
+    context.Response.Headers.Append("Cross-Origin-Resource-Policy", "same-site");
+    context.Response.Headers.Append("Referrer-Policy", "no-referrer");
+    context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+    context.Response.Headers.Append("Permissions-Policy", "camera=(), geolocation=(), magnetometer=(), microphone=(), payment=(), usb=()");
+    await next();
+});
+
+app.UseHttpsRedirection();
 app.MapControllers();
 app.UseStaticFiles();
 
